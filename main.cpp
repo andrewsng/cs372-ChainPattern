3786e37e325f053f0e88d4a992e3fc9c818a136b
Andrew Ng <ngandrew1337@gmail.com>
2021-04-29 03:46:41 -0800

    Write example code in main.cpp

--- main.cpp
+++ main.cpp
@@ -13,4 +13,20 @@ int main()
     std::cout << nickel->getValue() << "\n";
     std::cout << dime->getValue() << "\n";
     std::cout << quarter->getValue() << "\n";
+    
+    std::cout << "\n";
+    
+    auto pennyHandler = makePennyHandler();
+    auto nickelHandler = makeNickelHandler();
+    auto dimeHandler = makeDimeHandler();
+    auto quarterHandler = makeQuarterHandler();
+    
+    pennyHandler->setNext(nickelHandler);
+    nickelHandler->setNext(dimeHandler);
+    dimeHandler->setNext(quarterHandler);
+    
+    pennyHandler->handle(penny);
+    pennyHandler->handle(nickel);
+    pennyHandler->handle(dime);
+    pennyHandler->handle(quarter);
 }
